77c2b742a2a62f268970278e72da5c2b2b5dd330
chacox <chaco.g@gmail.com>
2015-05-24 14:15:14 -0430

    mach_msm: added CPU Overclock (OC) and Underclock (UC) support

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -47,10 +47,25 @@
 #define PRI_SRC_SEL_HFPLL	1
 #define PRI_SRC_SEL_HFPLL_DIV2	2
 
+#define SECCLKAGD		BIT(4)
 #ifdef CONFIG_SEC_DEBUG_SUBSYS
 int boost_uv;
 int speed_bin;
 int pvs_bin;
+#endif
+
+#ifdef CONFIG_CPU_OVERCLOCK
+#ifdef CONFIG_CPU_UNDERCLOCK
+#define FREQ_TABLE_SIZE    44
+#else
+#define FREQ_TABLE_SIZE    39
+#endif
+#else
+#ifdef CONFIG_CPU_UNDERCLOCK
+#define FREQ_TABLE_SIZE    39
+#else
+#define FREQ_TABLE_SIZE    35
+#endif
 #endif
 
 static DEFINE_MUTEX(driver_lock);
@@ -91,10 +106,20 @@ static void __cpuinit set_sec_clk_src(struct scalable *sc, u32 sec_src_sel)
 {
 	u32 regval;
 
+	/* 8064 Errata: disable sec_src clock gating during switch. */
 	regval = get_l2_indirect_reg(sc->l2cpmr_iaddr);
+	regval |= SECCLKAGD;
+	set_l2_indirect_reg(sc->l2cpmr_iaddr, regval);
+
+	/* Program the MUX */
 	regval &= ~(0x3 << 2);
 	regval |= ((sec_src_sel & 0x3) << 2);
 	set_l2_indirect_reg(sc->l2cpmr_iaddr, regval);
+
+	/* 8064 Errata: re-enabled sec_src clock gating. */
+	regval &= ~SECCLKAGD;
+	set_l2_indirect_reg(sc->l2cpmr_iaddr, regval);
+
 	/* Wait for switch to complete. */
 	mb();
 	udelay(1);
@@ -930,8 +955,60 @@ static void __init bus_init(const struct l2_level *l2_level)
 		dev_err(drv.dev, "initial bandwidth req failed (%d)\n", ret);
 }
 
+#define MAX_VDD 1300
+#define MIN_VDD 700
+
+ssize_t acpuclk_get_vdd_levels_str(char *buf)
+{
+    
+	int i, len = 0;
+    
+	if (buf) {
+		for (i = 0; drv.acpu_freq_tbl[i].speed.khz; i++) {
+            if (drv.acpu_freq_tbl[i].use_for_scaling) {
+                len += sprintf(buf + len, "%lumhz: %i mV\n",
+                           drv.acpu_freq_tbl[i].speed.khz/1000,
+                           drv.acpu_freq_tbl[i].vdd_core/1000 );
+            }
+		}
+	}
+	return len;
+}
+
+ssize_t acpuclk_set_vdd(char *buf)
+{
+	unsigned int cur_volt;
+	char count[10];
+	int i;
+    int ret = 0;
+    
+	if (!buf)
+		return -EINVAL;
+    
+	for (i = 0; i < drv.acpu_freq_tbl[i].speed.khz; i++) {
+        if (drv.acpu_freq_tbl[i].use_for_scaling) {
+            ret = sscanf(buf, "%d", &cur_volt);
+        
+            if (ret != 1)
+                return -EINVAL;
+        
+            if (cur_volt > MAX_VDD) {
+                cur_volt = MAX_VDD;
+            } else if (cur_volt < MIN_VDD) {
+                cur_volt = MIN_VDD;
+            }
+        
+            drv.acpu_freq_tbl[i].vdd_core = cur_volt*1000;
+                
+            ret = sscanf(buf, "%s", count);
+            buf += (strlen(count)+1);
+        }
+	}
+	return ret;
+}
+
 #ifdef CONFIG_CPU_FREQ_MSM
-static struct cpufreq_frequency_table freq_table[NR_CPUS][35];
+static struct cpufreq_frequency_table freq_table[NR_CPUS][FREQ_TABLE_SIZE];
 extern int console_batt_stat;
 static void __init cpufreq_table_init(void)
 {
